a8aeb536d5bd1ef8a4bfc487cd4178a2e63279d7
zrh <zhangrayhung@163.com>
2024-11-12 18:14:03 +0800

    add project asteroids

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color="black")
+        # refresh the screen
+        pygame.display.flip() 
+
+if __name__ == "__main__":
+    main()
